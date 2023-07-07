aa9b47d09e55c180768919cee31be67cf322332f
ShalienAutomationGIT <shalienkaushik06@gmail.com>
2023-07-07 15:03:06 +0530

    This is my first commit through terminal commands

---
+++ src/GITThroughTerminalCommands/Test1.java
@@ -0,0 +1,18 @@
+package GITThroughTerminalCommands;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		
+		String str = "GIT Practise using terminal commands";
+		
+		System.out.println(str);
+		
+		
+	}
+	
+	
+	
+	
+}
