54d850fcca91d46aac03ee9b65759724d16351ac
ShalienAutomationGIT <shalienkaushik06@gmail.com>
2023-07-07 18:43:14 +0530

    Newly created branch by Tester

---
+++ src/GITThroughTerminalCommands/Classcreatedbynewbranch.java
@@ -0,0 +1,21 @@
+package TestGIT;
+
+public class Classcreatedbynewbranch {
+
+	
+public static void main(String[] args) {
+		
+		
+		String str = "This class is created by new branch by Tester";
+		
+		System.out.println(str);
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+}
